0a8d6d855b9de79fd1fec2284559828afe703b7e
Dspolavori <sala01@targettrust.local>
2017-05-08 12:03:41 -0300

    aula1

---
+++ CursoAndroid/app/src/main/java/com/example/sala01/Aula01.java
@@ -0,0 +1,15 @@
+package com.example.sala01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.sala01.cursoandroid.R;
+
+public class Aula01 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aula01);
+    }
+}
